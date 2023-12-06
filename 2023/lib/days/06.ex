844b771701840ec7b35670b5fb3d1cf563bd5251
Krapaince <Krapaince@pm.me>
2023-12-06 10:21:32 +0100

    Add day06 part2.

--- 2023/lib/days/06.ex
+++ 2023/lib/days/06.ex
@@ -2,6 +2,19 @@ defmodule Day06 do
   def part1() do
     AdventOfCode2023.get_lines("6")
     |> parse_input()
+    |> run()
+  end
+
+  def part2() do
+    AdventOfCode2023.get("6")
+    |> String.replace(" ", "")
+    |> String.split("\n", trim: true)
+    |> parse_input()
+    |> run()
+  end
+
+  def run(races) do
+    races
     |> Enum.map(fn {time, record_distance} ->
       compute_every_possible_race(time)
       |> Stream.reject(fn race_distance -> race_distance <= record_distance end)
