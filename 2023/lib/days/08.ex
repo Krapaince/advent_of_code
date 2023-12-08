6c8f07d77d66f805ed5e39cb1f925924d129565d
Krapaince <Krapaince@pm.me>
2023-12-08 17:35:45 +0100

    Add day08 part2.

--- 2023/lib/days/08.ex
+++ 2023/lib/days/08.ex
@@ -12,6 +12,42 @@ defmodule Day08 do
     |> Kernel.+(1)
   end
 
+  def part2() do
+    {instructions, map} =
+      AdventOfCode2023.get("8")
+      |> parse_map()
+
+    true = do_nodes_ending_with_a_wrap_after_z?(instructions, map)
+
+    map
+    |> Enum.filter(fn {node_id, _} -> String.ends_with?(node_id, "A") end)
+    |> Enum.map(fn node ->
+      stream_steps(node, instructions, map)
+      |> Stream.take_while(fn node -> not String.ends_with?(node, "Z") end)
+      |> Enum.count()
+      |> Kernel.+(1)
+    end)
+    |> Day08.Math.lcm()
+  end
+
+  def do_nodes_ending_with_a_wrap_after_z?(instructions, map) do
+    map
+    |> Enum.filter(fn {node_id, _} -> String.ends_with?(node_id, "A") end)
+    |> Enum.all?(fn node ->
+      before_z =
+        stream_steps(node, instructions, map)
+        |> Enum.take_while(&String.ends_with?(&1, "Z"))
+
+      after_z =
+        stream_steps(node, instructions, map)
+        |> Stream.drop_while(&String.ends_with?(&1, "Z"))
+        |> Stream.drop(1)
+        |> Enum.take_while(&String.ends_with?(&1, "Z"))
+
+      before_z == after_z
+    end)
+  end
+
   def parse_map(content) do
     [instructions, map] = String.split(content, "\n\n", trim: true)
 
