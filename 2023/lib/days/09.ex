90f5c8a0b27dc42b92249d7eb10ed157b9b9b480
Krapaince <Krapaince@pm.me>
2023-12-09 09:59:24 +0100

    Add day09 part2.

--- 2023/lib/days/09.ex
+++ 2023/lib/days/09.ex
@@ -1,8 +1,11 @@
 defmodule Day09 do
-  def part1() do
+  def part1(), do: run(:forward)
+  def part2(), do: run(:backward)
+
+  def run(mode) do
     AdventOfCode2023.get_lines("9")
     |> parse_report()
-    |> Stream.map(&predict_next_step/1)
+    |> Stream.map(&predict_step(&1, mode))
     |> Enum.sum()
   end
 
@@ -15,15 +18,21 @@ defmodule Day09 do
     end)
   end
 
-  def predict_next_step(sequence) do
+  def predict_step(sequence, direction) do
     if Enum.all?(sequence, &Kernel.==(&1, 0)) do
       0
     else
       intervals = sequence |> Enum.chunk_every(2, 1, :discard) |> Enum.map(fn [a, b] -> b - a end)
 
-      next_step = predict_next_step(intervals)
+      next_step = predict_step(intervals, direction)
 
-      List.last(sequence) + next_step
+      case direction do
+        :forward ->
+          List.last(sequence) + next_step
+
+        :backward ->
+          hd(sequence) - next_step
+      end
     end
   end
 end
